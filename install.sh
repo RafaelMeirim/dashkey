29b09f86ebf87e73c7c57e37a6e848b511cd7c77
Rafael Meirim <meirim.rafael@gmail.com>
2026-06-14 16:10:19 -0300

    Update download paths in install.sh
    Change output paths for downloaded files to root directory.

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ cd "$INSTALL_DIR"
 
 curl -sO https://raw.githubusercontent.com/rafaelmeirim/dashkey/main/config.js
 curl -sO https://raw.githubusercontent.com/rafaelmeirim/dashkey/main/index.html
-curl -s https://raw.githubusercontent.com/rafaelmeirim/dashkey/main/data/links.js -o data/links.js
-curl -s https://raw.githubusercontent.com/rafaelmeirim/dashkey/main/images/background.jpg -o images/background.jpg
-curl -s https://raw.githubusercontent.com/rafaelmeirim/dashkey/main/icons/favicon.ico -o icons/favicon.ico
+curl -s https://raw.githubusercontent.com/rafaelmeirim/dashkey/main/data/links.js -o /links.js
+curl -s https://raw.githubusercontent.com/rafaelmeirim/dashkey/main/images/background.jpg -o /background.jpg
+curl -s https://raw.githubusercontent.com/rafaelmeirim/dashkey/main/icons/favicon.ico -o /favicon.ico
 curl -sO https://raw.githubusercontent.com/rafaelmeirim/dashkey/main/docker-compose.yml
 
 docker compose up -d
